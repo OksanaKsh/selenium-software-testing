94380430a765476f051534ca5452a73e9d114ec3
OksanaKsh <kshan.oksana@gmail.com>
2022-04-28 21:08:54 +0300

    Initial commit - add task1 - launch Browser

---
+++ SeleniumWebDriverCourse/Task1_LaunchChromeBrowser/UnitTest1.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace FirstProject
+{
+    public class OpenURLAndClose
+    {
+        IWebDriver webDriver;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            webDriver = new ChromeDriver("C:/SeleniumWebDriver/ABarantcevCource/SeleniumWebDriverCourse/SeleniumWebDriverCourse");
+            webDriver.Url = "https://gcatch.in/open-url-using-selenium-chromedriver-c/";
+            webDriver.Navigate();
+        }
+
+        [Test]
+        public void Test()
+        {
+            webDriver.Navigate().GoToUrl(webDriver.Url);
+            //Assert.Pass();
+            Thread.Sleep(2000);
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            webDriver.Close();
+        }
+    }
+}
